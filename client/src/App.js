14413ad6e02e234ad472f468af9a96449f97bbe8
Hugo Cullinan <hugo.cullinan@gmail.com>
2024-01-26 15:11:29 +1100

    consume server api and render to front end client

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
-  return <div></div>;
+  const [backendData, setBackendData] = useState([{}]);
+
+  useEffect(() => {
+    fetch("/api")
+      .then((response) => response.json())
+      .then((data) => {
+        setBackendData(data);
+      });
+  }, []);
+
+  return (
+    <div>
+      {typeof backendData.cars === "undefined" ? (
+        <p>Loading...</p>
+      ) : (
+        backendData.cars.map((car, idx) => <p key={idx}>{car}</p>)
+      )}
+    </div>
+  );
 }
 
 export default App;
