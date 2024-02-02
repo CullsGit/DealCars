bc9f26fbb804135f8fc920236a77be3e4ac9b6e4
Hugo Cullinan <hugo.cullinan@gmail.com>
2024-02-03 09:16:56 +1100

    responsive design for navbar

--- client/src/components/hero/Hero.jsx
+++ client/src/components/hero/Hero.jsx
@@ -4,16 +4,10 @@ import "./hero.css";
 const Hero = () => {
   return (
     <>
-      <section id="home" className="hero-section">
-        <div className="container">
-          <div className="hero-content">
-            <h1>
-              Introducing the <span>Future</span> of Driving Excellence
-            </h1>
-            <h2>Explore the Viper GZ: A Symphony of Innovation and Style</h2>
-          </div>
-        </div>
-      </section>
+      {/* <h1>
+        Introducing the <span>Future</span> of Driving Excellence
+      </h1>
+      <h2>Explore the Viper GZ: A Symphony of Innovation and Style</h2> */}
     </>
   );
 };
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,9 +1,28 @@
-import React from "react";
-import { RiMenu3Line, RiCloseLin } from "react-icons/ri";
+import React, { useState } from "react";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import "./navbar.css";
 import logo from "../../assets/logo.png";
 
+const Menu = () => (
+  <>
+    <p>
+      <a href="#link">Link</a>
+    </p>
+    <p>
+      <a href="#link">Link</a>
+    </p>
+    <p>
+      <a href="#link">Link</a>
+    </p>
+    <p>
+      <a href="#link">Link</a>
+    </p>
+  </>
+);
+
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <div className="dealcars__navbar">
       <div className="dealcars__navbar-links">
@@ -13,19 +32,30 @@ const Navbar = () => {
           </a>
         </div>
         <div className="dealcars__navbar-links_container">
-          <p>
-            <a href="#link">Link</a>
-          </p>
-          <p>
-            <a href="#link">Link</a>
-          </p>
-          <p>
-            <a href="#link">Link</a>
-          </p>
-          <p>
-            <a href="#link">Link</a>
-          </p>
+          <Menu />
         </div>
+      </div>
+      <div className="dealcars__navbar-menu">
+        {toggleMenu ? (
+          <RiCloseLine
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <RiMenu3Line
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+        {toggleMenu && (
+          <div className="dealcars__navbar-menu_container scale-up-center">
+            <div className="dealcars__navbar-menu_container-links">
+              <Menu />
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
