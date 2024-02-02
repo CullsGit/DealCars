dd9f655e17688225eea085e19ed9f79a5ebef652
Hugo Cullinan <hugo.cullinan@gmail.com>
2024-02-02 11:45:23 +1100

    add brand logo and navbar with basic styling

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,10 +1,32 @@
 import React from "react";
+import { RiMenu3Line, RiCloseLin } from "react-icons/ri";
 import "./navbar.css";
+import logo from "../../assets/logo.png";
 
 const Navbar = () => {
   return (
-    <div>
-      <p>Navbar</p>
+    <div className="dealcars__navbar">
+      <div className="dealcars__navbar-links">
+        <div className="dealcars__navbar-links_logo">
+          <a href="#link">
+            <img src={logo} alt="logo" />
+          </a>
+        </div>
+        <div className="dealcars__navbar-links_container">
+          <p>
+            <a href="#link">Link</a>
+          </p>
+          <p>
+            <a href="#link">Link</a>
+          </p>
+          <p>
+            <a href="#link">Link</a>
+          </p>
+          <p>
+            <a href="#link">Link</a>
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
